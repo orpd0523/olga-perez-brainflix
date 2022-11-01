18da041d13717acaa3e6ca63c23a944334541486
orpd0523 <orpd0523@gmail.com>
2022-10-31 21:12:35 -0500

    created components for Video Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
-import Navigation from './components/NavigationSection/Navigation';  
+import Navigation from './components/NavigationSection/Navigation'; 
+import Video from './components/VideoSection/Video'; 
 
 function App() {
   return (
     <div className="App">
     <Navigation/>
+    <Video/>
     </div>
   );
 }
---
+++ src/components/VideoSection/Video.js
@@ -0,0 +1,13 @@
+import "./Video.scss"
+
+function Video() {
+    return(
+        <section>
+            <video>
+                <source></source>
+            </video>
+        </section>
+    )
+}
+
+export default Video;
