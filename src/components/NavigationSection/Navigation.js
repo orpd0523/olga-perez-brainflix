93cd47cc6f7e5edfd398fb2afda368498b5a8ab1
orpd0523 <orpd0523@gmail.com>
2022-11-06 20:45:37 -0600

    completed nav bar

--- src/components/NavigationSection/Navigation.js
+++ src/components/NavigationSection/Navigation.js
@@ -2,7 +2,7 @@ import "./Navigation.scss";
 import logoImage from "../../assets/images/BrainFlix-logo.svg";
 import search from "../../assets/icons/search.svg";
 import upload from "../../assets/icons/upload.svg";
-import Button from "../Button/Button"
+import Button from "../Button/Button";
 import Avatar from "../Avatar/Avatar";
 import weekend from "../../assets/images/Mohan-muruge.jpg";
 import TextField from "../TextField/TextField";
@@ -11,12 +11,26 @@ function Navigation() {
   return (
     <header className="container">
       <nav className="navigation">
-        <div className="navigation__logo-container">
-          <img className="navigation__logo"alt="brain flix logo" src={logoImage} />
-        </div>
-        <TextField alt="search icon" src={search} placeholder="Search"/>
-        <Button alt="upload" src={upload}>UPLOAD</Button>
-        <Avatar alt="avatar" src={weekend}/>
+        <span className="navigation__logo-container">
+          <img
+            className="navigation__logo"
+            alt="brain flix logo"
+            src={logoImage}
+          />
+        </span>
+        <span className="navigation__main">
+          <span className="navigation__search">
+            <TextField alt="search icon" src={search} placeholder="Search" />
+          </span>
+          <span className="navigation__avatar">
+            <Avatar alt="avatar" src={weekend} />
+          </span>
+          <span className="navigation__upload">
+            <Button alt="upload" src={upload}>
+              UPLOAD
+            </Button>
+          </span>
+        </span>
       </nav>
     </header>
   );
