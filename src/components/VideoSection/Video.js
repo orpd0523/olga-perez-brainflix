bf8dcd5d24a6807544b1b2f4e25015c55aa216ea
orpd0523 <orpd0523@gmail.com>
2022-11-06 22:35:26 -0600

    styled video controls

--- src/components/VideoSection/Video.js
+++ src/components/VideoSection/Video.js
@@ -1,4 +1,8 @@
 import "./Video.scss";
+import playIcon from "../../assets/icons/play.svg"
+import fullScreenIcon from '../../assets/icons/fullscreen.svg'
+import volumeUpIcon from '../../assets/icons/volume_up.svg';
+
 
 function Video(props) {
   const { image } = props;
@@ -8,9 +12,30 @@ function Video(props) {
         <video poster={image} className="video__screen">
           <source></source>
         </video>
+        <VideoControls {...props}/>
       </div>
     </section>
   );
+}
+
+function VideoControls(props){
+  return(
+    <div className='video__controls'>
+      <span className='video__control'>
+        <img className='video__icon' alt='play' src={playIcon}/>
+      </span>
+      <span className='video__control video__control--center'>
+        <span className='video__progress-bar'></span>
+        <span className='video__time'>
+        0:00/{props.duration}
+        </span>
+      </span>
+      <span className='video__control'>
+        <img className='video__icon' alt='fullscreen' src={fullScreenIcon}/>
+        <img className='video__icon' alt='volume up' src={volumeUpIcon}/>
+      </span>
+    </div>
+  )
 }
 
 export default Video;
