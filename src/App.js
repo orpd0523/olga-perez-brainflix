310b15392f5a49a47b2cfb1cb8000a745e5849e0
orpd0523 <orpd0523@gmail.com>
2022-11-13 16:11:26 -0600

    adding useEffect and axios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
 import "./App.scss";
 import Navigation from "./components/NavigationSection/Navigation";
 import videoDetailsData from "./data/video-details.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import VideoPage from "./components/VideoPage/VideoPage";
 import UploadVideo from "./components/UploadVideo/UploadVideo";
+import axios from 'axios';
 
-function App() {
-  const [videoDetails] = useState(videoDetailsData);
-  const [currentVideo, setCurrentVideo] = useState(videoDetails[0]);
-  function updateCurrentVideo(video) {
-    const selectedVideo = videoDetails.find((vid) => {
-      return vid.id === video.id;
-    });
-    setCurrentVideo(selectedVideo);
-  }
+  function App() {
+    useEffect(() =>{
+      axios.get('https://project-2-api.herokuapp.com')
+        .then(response =>{
+        console.log(response)
+      });
+    }, []);
+//   const [videoDetails] = useState(videoDetailsData); 
+//   const [currentVideo, setCurrentVideo] = useState(videoDetails[0]);
+//   function updateCurrentVideo(video) {
+//     const selectedVideo = videoDetails.find((vid) => {
+//       return vid.id === video.id;
+//     });
+//     //
+    //setCurrentVideo(selectedVideo);
+  //}
   return (
     <BrowserRouter>
       <div className="app">
         <Navigation />
         <Routes>
-          <Route
+          {/* <Route
             path="/"
             element={
               <VideoPage
-                currentVideo={currentVideo}
-                updateCurrentVideo={updateCurrentVideo}
+                // currentVideo={currentVideo}
+                // updateCurrentVideo={updateCurrentVideo}
               />
             }
           ></Route>
@@ -34,12 +42,12 @@ function App() {
             path="videos/:videoid"
             element={
               <VideoPage
-                currentVideo={currentVideo}
-                updateCurrentVideo={updateCurrentVideo}
+                // currentVideo={currentVideo}
+                // updateCurrentVideo={updateCurrentVideo}
               />
             }
-          ></Route>
-        </Routes>
+          ></Route>*/}
+        </Routes> 
       </div>
     </BrowserRouter>
   );
