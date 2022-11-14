9ce9afcdf1409325934c17417b5f1187d8b19dc2
orpd0523 <orpd0523@gmail.com>
2022-11-13 20:37:17 -0600

    styling buttons and upload form mobile

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,9 @@
 import "./Button.scss";
 
 function Button(props) {
-  const { alt, src, children } = props;
+  const { alt, src, children, color = "primary" } = props;
   return (
-    <button className="button">
+    <button className= {"button button--" + color}>
       <span className="button__prepend">
         <img className="button__icon" alt={alt} src={src} />
       </span>
--- src/components/UploadVideo/UploadVideo.js
+++ src/components/UploadVideo/UploadVideo.js
@@ -2,6 +2,7 @@ import Button from "../Button/Button";
 import TextField from "../TextField/TextField";
 import publish from "../../assets/icons/publish.svg";
 import videoPreview from "../../assets/images/Upload-video-preview.jpg";
+import "./UploadVideo.scss"
 
 function UploadVideo() {
   return (
@@ -14,20 +15,20 @@ function UploadVideo() {
 
 function UploadForm(props) {
   return (
-    <form>
-      <div>
-        <span>
+    <form className="upload-form">
+      <div className="upload-form__body">
+        <span className="upload-form__aside">
           <small className="typography typography--secondary">VIDEO THUMBNAIL</small>
-          <img alt="video preview" src={videoPreview} />
+          <img className="upload-form__preview" alt="video preview" src={videoPreview} />
         </span>
-        <span>
-          <TextField label="TITLE YOUR VIDEO" />
-          <TextField label="ADD A VIDEO DESCRIPTION" />
+        <span className="upload-form__main">
+          <TextField label="TITLE YOUR VIDEO" placeholder="Add a title to your video"/>
+          <TextField label="ADD A VIDEO DESCRIPTION"  placeholder="Add a description to your video" />
         </span>
       </div>
-      <div>
-        <Button>CANCEL</Button>
+      <div className="upload-form__button-group">
         <Button alt="publish" src={publish}>PUBLISH</Button>
+        <Button color = "secondary">CANCEL</Button>
       </div>
     </form>
   );
