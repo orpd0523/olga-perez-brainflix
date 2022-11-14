e27e31aa1029242d6e94369d79de03f1b07888c6
orpd0523 <orpd0523@gmail.com>
2022-11-13 21:08:27 -0600

    styling and rsponsiveness

--- src/components/UploadVideo/UploadVideo.js
+++ src/components/UploadVideo/UploadVideo.js
@@ -2,14 +2,16 @@ import Button from "../Button/Button";
 import TextField from "../TextField/TextField";
 import publish from "../../assets/icons/publish.svg";
 import videoPreview from "../../assets/images/Upload-video-preview.jpg";
-import "./UploadVideo.scss"
+import "./UploadVideo.scss";
 
 function UploadVideo() {
   return (
-    <section className="container">
-      <h1 className="typography typography--h1">Upload Video</h1>
-      <UploadForm/>
-    </section>
+    <main className="upload-section">
+      <section className="container ">
+        <h1 className="typography typography--h1">Upload Video</h1>
+        <UploadForm />
+      </section>
+    </main>
   );
 }
 
@@ -18,17 +20,35 @@ function UploadForm(props) {
     <form className="upload-form">
       <div className="upload-form__body">
         <span className="upload-form__aside">
-          <small className="typography typography--secondary">VIDEO THUMBNAIL</small>
-          <img className="upload-form__preview" alt="video preview" src={videoPreview} />
+          <small className="typography typography--secondary">
+            VIDEO THUMBNAIL
+          </small>
+          <img
+            className="upload-form__preview"
+            alt="video preview"
+            src={videoPreview}
+          />
         </span>
         <span className="upload-form__main">
-          <TextField label="TITLE YOUR VIDEO" placeholder="Add a title to your video"/>
-          <TextField label="ADD A VIDEO DESCRIPTION"  placeholder="Add a description to your video" />
+          <TextField
+            label="TITLE YOUR VIDEO"
+            placeholder="Add a title to your video"
+          />
+          <TextField
+            label="ADD A VIDEO DESCRIPTION"
+            placeholder="Add a description to your video"
+          />
         </span>
       </div>
       <div className="upload-form__button-group">
-        <Button alt="publish" src={publish}>PUBLISH</Button>
-        <Button color = "secondary">CANCEL</Button>
+        <span>
+          <Button alt="publish" src={publish}>
+            PUBLISH
+          </Button>
+        </span>
+        <span>
+          <Button color="secondary">CANCEL</Button>
+        </span>
       </div>
     </form>
   );
