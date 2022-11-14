62c7d4678e5c9858fc9b525273487fa3c22c4271
orpd0523 <orpd0523@gmail.com>
2022-11-13 22:13:41 -0600

    real final commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "../src/styles/globals.scss";
 import App from "./App";
 
